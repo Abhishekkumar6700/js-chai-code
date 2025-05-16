16167d76f9d0eabf3dddc3ef02beda12ae1ef69a
Abhishekkumar6700 <110298957+Abhishekkumar6700@users.noreply.github.com>
2025-05-16 07:30:57 +0000

    variable push

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountId=144553
+let email ="avvi@gmail.com"
+var password="26542"
+//accountCity ="noida"
+email="suhgs@gmail.com"
+password="48748"
+accountCity="jaipur"
+console.log(accountId)
+console.table([accountId, email, password,accountCity])
