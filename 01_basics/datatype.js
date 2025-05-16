d0d704fb92a5e81cd8ebe2b9ebebcb0df994fbe5
Abhishekkumar6700 <110298957+Abhishekkumar6700@users.noreply.github.com>
2025-05-16 16:27:47 +0000

    datatype summmary

---
+++ 01_basics/datatype.js
@@ -0,0 +1,30 @@
+// Primitive
+ 
+// 7 types :String ,Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const isLogged = false
+const outsideTemp = null
+let email;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+ console.log( id === anotherId)
+
+ const bigNumber = 4546576767n
+
+// Reference (Non primitive) //memory me reference direct allocate kiya ja skta hai
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman" , "nagraj", "doga"]
+
+let myObj{
+    name: "abhi",
+    age:22,
+
+};
+
+const myFunction = function(){
+    console.log("helllo");
+}
