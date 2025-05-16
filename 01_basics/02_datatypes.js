bfa49ea9906b713b51d6c90e018dd306410e3044
Abhishekkumar6700 <110298957+Abhishekkumar6700@users.noreply.github.com>
2025-05-16 08:31:29 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+ "use strict"; //treat all js code as newer version 
+
+ console .log("hello")
+
+ let name="abhi"
+ let age= 18
+ let isLoggedIn= false
+ let state;
+ 
+ // number => 2 to power 53
+ // bigint
+ // string=> ""
+ // boolean=> true/false
+ // null => standalone value
+ // undefined => values doesnot assign
+ // symbol => unique
+
+
+ //object
+
+ console.log(typeof "hi")
