48463081622103b0e6892d3db1e966dd433a2a0c
Abhishekkumar6700 <110298957+Abhishekkumar6700@users.noreply.github.com>
2025-05-17 11:21:40 +0000

    datetime

---
+++ 01_basics/datetime.js
@@ -0,0 +1,19 @@
+//Date
+
+let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toISOString())
+//console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+
+//let myCreatedDate = new Date (2023,0 ,12)
+//console.log(myCreatedDate.toDateString());
+
+// let myTime = Date.now()
+// console.log(myTime);
+// console.log(Date.now())
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getDate());
