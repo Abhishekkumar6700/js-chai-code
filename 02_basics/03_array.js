8f13e94b51d08bd27179d4c6a5466977abedeebe
Abhishekkumar6700 <110298957+Abhishekkumar6700@users.noreply.github.com>
2025-05-18 09:02:13 +0000

    arraybas

---
+++ 02_basics/03_array.js
@@ -0,0 +1,12 @@
+
+//fill 
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.fill("Kiwi");
+// console.log(fruits);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.fill("Kiwi", 2, 4); //overwrite the array
+// console.log(fruits)
+
+
