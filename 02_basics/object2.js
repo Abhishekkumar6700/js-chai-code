e397086d55ea6cd2a948d777a83bc0efb74b65d1
Abhishekkumar6700 <110298957+Abhishekkumar6700@users.noreply.github.com>
2025-05-18 09:01:52 +0000

    object2

---
+++ 02_basics/object2.js
@@ -0,0 +1,26 @@
+const tinderUser = new Object()
+
+tinderUser.id = "123abc"
+tinderUser.name="sam"
+tinderUser.isLogged=  false
+
+//console.log(tinderUser)
+
+const regularUser = {
+    email: "abhis@gmail.com",
+    fullname: {
+        name:{
+        username: "rohit",
+        lastname: "singh"
+    }
+}
+}
+//console.log(regularUser.fullname.name.lastname)
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 ={3: "c" ,4:"d"}
+
+// const obj3 = Object.assign({},obj1 ,obj2)
+// const obj4= {...obj1, ...obj2}
+// console.log(obj3)
+// console.log(obj4)
