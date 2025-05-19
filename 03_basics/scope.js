930326c44becc7ac5cf660103754f7c14a8a050c
Abhishekkumar6700 <110298957+Abhishekkumar6700@users.noreply.github.com>
2025-05-19 04:47:09 +0000

    scope

---
+++ 03_basics/scope.js
@@ -0,0 +1,11 @@
+var c =300
+
+if(true){
+    let a = 10
+    const b=29
+    var c=298
+}
+
+//console.log(a); //error let or const local scope se bahar nhi jata hai
+//console.log(b);
+console.log(c);
