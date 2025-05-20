0b267c2e827f89dbf63bf2f08f550f4f99c39836
Abhishekkumar6700 <110298957+Abhishekkumar6700@users.noreply.github.com>
2025-05-20 04:00:59 +0000

    modified scope

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -1,11 +1,56 @@
-var c =300
+// var c =300
 
-if(true){
-    let a = 10
-    const b=29
-    var c=298
-}
+// if(true){
+//     let a = 10
+//     const b=29
+//     var c=298
+// }
 
-//console.log(a); //error let or const local scope se bahar nhi jata hai
-//console.log(b);
-console.log(c);
+ //console.log(a); //error let or const local scope se bahar nhi jata hai 
+ //console.log(b);
+// console.log(c);
+
+// function one() {
+//   const name = "abhi" 
+//   function two(){ 
+//     const website = "youtube"
+//     console.log(name);
+//   }
+//   //console.log(website) => error 
+//   two();
+// }
+
+
+// one();
+
+// if(true) {
+//     const username ="hitesh"
+//    if(username === "hitesh"){
+//     const website = "youtube"
+//     console.log(username+website)
+//    }
+// }
+
+// const user = {
+//      username: "hitesh",
+//      price:999,
+
+//      welcomeMessage:function() {
+//         console.log(`${this.username} ,welcome to website`)
+//      }
+// }
+
+
+// ++++++++++++++++++++ interesting ++++++++++++++++
+
+// function addone(num){
+//     return num+1
+// }
+// addone(5)
+
+
+
+// const addtwo = function(num){
+//     return num+2
+// }
+// addtwo(4)
