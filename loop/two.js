305bdd4321075eaa33deac13e6afe61e21a09a93
Abhishekkumar6700 <110298957+Abhishekkumar6700@users.noreply.github.com>
2025-05-20 06:43:30 +0000

    forin

---
+++ loop/two.js
@@ -0,0 +1,27 @@
+const myobj = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+//  for (const key in myobj){
+//     console.log(key); //js,cpp,rb,swift
+//  }
+
+//  for (const key in myobj){
+//     console.log(myobj[key]); //javascript,,c++, ...
+//  }
+
+
+//*********for in loop in Array**********
+
+const programming =["js" ,"rb", "py", "java" ]
+
+// for(const key in programming){
+//     console.log(key);
+// }
+
+// for(const key in programming){
+//     console.log(programming[key]); //value print 
+// }
